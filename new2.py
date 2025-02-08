08f4545d0997eb3209a68e6930a54666da4067b9
MADARA143390 <ragu26555@gmail.com>
2025-02-08 15:19:32 +0530

    files changed

--- new2.py
+++ new2.py
@@ -1,6 +1,12 @@
-m=100
-for a in range(1,m+1):
-    if m%a==0:
-        print(a)
-            
-        
+# m=100
+# for a in range(1,m+1):
+#     if m%a==0:
+#         print(a)
+m = 100
+count = 0
+while True:
+    count+=1
+    if m % count == 0:
+        print(count)
+    elif count == 101:
+        break
